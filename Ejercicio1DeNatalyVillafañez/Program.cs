5e45480b3cf5f49b4f229c9726f8663b8481c273
natalyvillafanez <natalyvillafanez@gmail.com>
2022-07-05 23:44:38 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio1DeNatalyVillafañez/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ejercicio1DeNatalyVillafañez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "INFORME DE DOLARES Y EUROS";
+
+            Console.WriteLine("Le informaremos la cantidad de Dólares que puede comprar...");
+            Console.WriteLine("Para ello necesitamos saber de cuánto dinero dispone.");
+            Console.Write("INGRESE LA CANTIDAD DE DINERO EN PESOS DISPONIBLE: ");
+            var pesosIngresados = double.Parse(Console.ReadLine());
+
+            var dolar = (pesosIngresados / 230);
+            var euro = (dolar * 1.17);
+
+            Console.WriteLine("-----------------------------------------------------------------");
+            Console.WriteLine("Con {0} Pesos, puede comprar {1} Dólares.", pesosIngresados, dolar);
+            Console.WriteLine("-----------------------------------------------------------------");
+            Console.WriteLine("Y con {0} Dólares, puede comprar {1} Euros.", dolar, euro);
+            Console.WriteLine("-----------------------------------------------------------------");
+        }
+    }
+}
